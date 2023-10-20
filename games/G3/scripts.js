ccc5d323bf01cb0bc844d8c9ef1d0cb6a30a2af1
BeetleBeast <78571657+BeetleBeast@users.noreply.github.com>
2023-10-20 20:47:04 +0200

    Update scripts.js

--- games/G3/scripts.js
+++ games/G3/scripts.js
@@ -1,6 +1,6 @@
 /* if there is no saved files create a new dave file
 */
-/*
+
 onload = funtion() {startup()};
 function startup(latest_loaded_game):
    if latest_loaded_game == '':
@@ -8,6 +8,6 @@ function startup(latest_loaded_game):
 // newGame makes the prep for a new game
 function newGame():
     //idk
-
+/*
 latest_loaded_game = savefile.latest // full wrong
 */
