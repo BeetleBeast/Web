42747502850512ff99d0b555010064c26f841f2d
BeetleBeast <xkeleter2@gmail.com>
2023-11-15 15:07:01 +0100

    m

--- games/G3/scripts.js
+++ games/G3/scripts.js
@@ -5,7 +5,7 @@ const bar = document.querySelector('.bar');
 const extra = document.querySelector('.extras');
 const options = document.querySelector('.options');
 
-
+const fs = require('fs');
 
 // vue app
 /*
@@ -47,24 +47,35 @@ function newGame(){
     var saveFile = {};
     localStorage.setItem('saveFileG3 '+saveFileNum, saveFile);
     story();
-    
+    document.createDocumentFragment
+    // TODO: set json file instead of localstorage (by using stringify and parce)
 
 }
 // load the latest game (goes to SG file to load and save the game)
+
 function loadGame(latest){
     //idk
     console.log('load game');
     saveFileNum = latest;
     
     localStorage.getItem('saveFileG3 '+saveFileNum);
-    
+
+    last_loaded_game = ''
+    return last_loaded_game
+    // TODO: set json file instead of localstorage (by using stringify and parce)
 }
 // save loaded game (goes to SG file to load and save the game)
 function savefile(saveFile){
     //idk
     console.log('saving game');
     saveFileNum = prompt('number savefile');
-    localStorage.setItem('saveFileG3 '+saveFileNum, saveFile);
+    // localStorage.setItem('saveFileG3 '+saveFileNum, saveFile); // FIXME: after json files made rzmove localstorage 
+    var saveFileJSON = JSON.stringify(saveFile)
+    fs.writeFile('saveFile'+saveFileNum+'.json', saveFileJSON, (err) => {
+        if (err) throw err;
+        console.log('JSON data is saved.');
+       });
+    // TODO: set json file instead of localstorage (by using stringify and parce)
 }
 
 // initial latest loaded game
@@ -89,7 +100,7 @@ function story(){
     main_section.innerHTML = story_0;
 
     var saveFile = {"saveFileNum" : saveFileNum,"story_Number" : 0, "title_story" : title_story_0, "choices_Made" : []}
-
+    return saveFile
 }
 
 document.querySelector('.Open-more').addEventListener('click', function() {
