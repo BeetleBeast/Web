2c4e5e7cdd4b9414fbe1f7f3b7ffa083c72f9de5
BeetleBeast <xkeleter2@gmail.com>
2024-03-23 21:10:45 +0100

    mid change

--- games/G3/scripts.js
+++ games/G3/scripts.js
@@ -17,6 +17,8 @@ var saveFileNum = 0;
 var last_loaded_game = '';
 var typeOfGame = 'New_Game';
 
+let isCurrentlyPrinting = false; // set true if is printing and false if not curently printing if true and printImmediately is also true stop slowTypingText and print everything 
+
 // initial latest loaded game
 if (saveFileNum == 0){
     saveFileNum = 1;
@@ -66,16 +68,49 @@ function newGame(saveFileNum){
     let saveFile = {
         "saveFileNumber" : saveFileNum,
         "curent_storyLine_progress" : 0,
+        "curent_chapter_progress" : 0,
+        "curent_nonlinear_storyLine_progress" : 0,
         "storyLine_progress" : {
-            0 :  {"sceneName": "Start", "playerText": "In the heart of a bustling metropolis lies the foundation of a towering ambition. Welcome to Tower's Edge, where strategic minds converge to erect the mightiest structures the skyline has ever witnessed. Assemble your team, fortify your defenses, and rise above the competition in this thrilling test of engineering prowess and tactical mastery. Will you reach new heights or crumble beneath the weight of your rivals' ascent? The choice is yours in Tower's Edge."},
-            1 :  {"sceneName": "First_step", "playerText": "Your first step in Tower's Edge begins with a single blueprint and a vision to reach the heavens. Survey the landscape, choose your plot wisely, and lay the cornerstone of your empire. As construction commences, every decision shapes the fate of your tower, from selecting materials to orchestrating the placement of each floor. With each level you ascend, challenges will emerge, but with cunning strategy and meticulous planning, you'll forge your path towards architectural supremacy. Welcome to the foundation of your legacy in Tower's Edge."},
+            0 :  {"sceneName" : "Start", "playerText": "You wake up in a dark cave, the damp air clinging to your skin as you grope for any semblance of direction. The sound of distant rumbling echoes through the cavern, urging you to explore. With each step, the path twists and turns, revealing ancient ruins and forgotten passages. As you navigate this mysterious labyrinth, the weight of uncertainty presses upon you, yet a flicker of curiosity ignites within."},
+            1 :  {"sceneName" : "charachterDefining-0", "playerText" : "Gazing into the rippling surface of a subterranean pool, you catch a glimpse of your own reflection amidst the murky depths. The dim light barely illuminates your features, leaving much to the imagination. Who is this figure staring back at you? The lines of gender blur in the wavering distortion, leaving only a sense of mystery in its wake. Lost in contemplation, you linger in the tranquil embrace of the cave, pondering the journey that lies ahead. With each ripple that disturbs your reflection, a surge of determination propels you forward, ready to embrace the enigma of your own existence. Let us now delve deeper into the story of this mysterious soul, as we unveil the intricate details of their identity amidst the shadows of the underworld."},
+            2 :  {"sceneName" : "charachterDefining-1", "playerText" : "Their eyes shimmered with a color that held steadfast"},
+            3 :  {"sceneName" : "charachterDefining-1", "playerText" : "Their hair framed their face in a style that could be described as"},
+            4 :  {"sceneName" : "charachterDefining-2", "playerText" : ""},
+            5 :  {"sceneName" : "charachterDefining-3", "playerText" : ""},
+            6 :  {"sceneName" : "charachterDefining-4", "playerText" : ""},
+            7 :  {"sceneName" : "charachterDefining-5", "playerText" : ""},
             /* ADD EXTRA SCENES */
+        },
+        "storyLine_nonlinear" :  {
+            0 : {"sceneName" : "First_choice", "playerText" : "placeholder placeholder placeholder placeholder placeholder placeholder "},
+            1 : {"sceneName" : "Second_choice", "playerText" : "placeholder placeholder placeholder placeholder placeholder placeholder "},
+            /* ADD EXTRA nonlinear SCENES */
+        },
+        "Chapter_num" : {
+            // chapter num : scene in chapter num
+            0 : [0,1,2,3,4,5,6,7],
+            /*  ADD EXTRA chapters and scene's in chapter*/
+        },
+        "Choices_Possible" : {
+            //  chapter number : { scene number : { Button option : Text }}
+            0 : {
+                0 : { 7 : "Next"},       //FIXME change all options from 5 to 7
+                1 : { 1 : "Previously", 7 : "Next",},
+                2 : { 1 : "Previously", 2 : "piercing blue", 3 : "deep brown", 4 : "striking green", 5 : "captivating hazel", 6 : "intense grey", 7 : "Next",}
+            },
+
+        },
+        "Choices_Made" : {
+            //  chapter number : string of Button NUM Choices made // EXAMPLE 1,5,3 first CH, fith CH , third CH
+            0 : [], 
+            /*  ADD EXTRA chapters*/
         },
         "Player_character" : Player,
         "Buttons_section_title" : {0 : " ",},
         "AmountOfButtonsForSceneDefault" : 2,
         "AmountOfButtonsForScene" : {
-            0: 1, // scene number : amount of buttons in that scene
+            // scene number : amount of buttons in that scene
+            0: 1, 
             1: 2,
         },
         "current_title_progress" : 0,
@@ -94,6 +129,7 @@ function newGame(saveFileNum){
         .then(() => console.log('File saved.'))
         .catch(err => console.error('Error saving file:', err));
     */
+
     // start story
     story(saveFile);
     
@@ -176,6 +212,8 @@ function story(saveFile){
     const Button_Choice3 = document.querySelector('.Sh_3');
     const Button_Choice4 = document.querySelector('.Sh_4');
     const Button_Choice5 = document.querySelector('.Sh_5');
+    const Button_Choice6 = document.querySelector('.Sh_6');
+    const Button_Choice7 = document.querySelector('.Sh_7');
     const savefileId = document.getElementById('savefileId');
 
 
@@ -194,27 +232,51 @@ function story(saveFile){
     scene_progress(current_storyLine,curent_storyLine_progress)
 
 
-    choices_section_title.innerHTML = saveFile.Buttons_section_title[0];  // change if needed be                                   
-
-    if (saveFile.AmountOfButtonsForScene[curent_storyLine_progress] == 1){
-        let Button = Button_Choice5;
-        Button.innerHTML = 'Next';
-        Button.addEventListener("click", function() {
-            nextScene(saveFile);
-        })
-    }
-    if (saveFile.AmountOfButtonsForScene[curent_storyLine_progress] == 2) {
+    choices_section_title.innerHTML = saveFile.Buttons_section_title[0];  // change if needed be
+    
+    if (saveFile.AmountOfButtonsForScene[curent_storyLine_progress] == saveFile.AmountOfButtonsForSceneDefault){
         let Button1 = Button_Choice1;
-        let Button2 = Button_Choice5;
-        Button2.innerHTML = 'Next';  
+        let Button7 = Button_Choice7;
+        Button7.innerHTML = 'Next';  
         Button1.innerHTML = 'Previously';
         Button1.addEventListener("click", function(){
-            previousScene(saveFile);
+            if(isCurrentlyPrinting){
+                slowTypingText(current_storyLine['playerText'], '.main_section',undefined,undefined,true)
+                slowTypingText(current_title['title_story_'+current_title_progress], '.main_section',undefined,undefined,true)
+            }else{
+                console.log("test id=257 ",saveFile.Choices_Made[saveFile.curent_chapter_progress]);
+                saveFile.Choices_Made[saveFile.curent_chapter_progress].pop();
+                console.log("test id=258 ",saveFile.Choices_Made[saveFile.curent_chapter_progress]);
+                previousScene(saveFile);
+            }
         })
-        Button2.addEventListener("click", function() {
-            nextScene(saveFile);
+        Button7.addEventListener("click", function() {
+            if(isCurrentlyPrinting){
+                slowTypingText(current_storyLine['playerText'], '.main_section',undefined,undefined,true)
+                slowTypingText(current_title['title_story_'+current_title_progress], '.main_section',undefined,undefined,true)
+            }else{
+                console.log("test id=259 ",saveFile.Choices_Made[saveFile.curent_chapter_progress]);
+                saveFile.Choices_Made[saveFile.curent_chapter_progress].push(5);
+                console.log("test id=260 ",saveFile.Choices_Made[saveFile.curent_chapter_progress]);
+                nextScene(saveFile);
+                
+            }
         })
+
+    }else{
+        ButtonPressed(saveFile)
     }
+    
+    /*
+    NOTES
+            "Chapter_num" : {
+            0 : [0,1],  // chapter num : scene in chapter num
+        },
+        "Choices_Made" : {
+
+         0 : [], //  chapter number : string of Button NUM Choices made // EXAMPLE 1,5,3 first CH, fith CH , third CH
+        },
+    */
 
     // save loaded game (goes to SG file to load and save the game)
     if (savefileId && !savefileId.hasAttribute('data-listener-added')) {
@@ -233,89 +295,133 @@ function story(saveFile){
       });
     
       savefileId.setAttribute('data-listener-added', 'true');
-    } else {
-      savefileId.removeEventListener("click", savefileId.addEventListener("click"));
     }
+    /* else {
+        savefileId.removeEventListener("click", savefileId.addEventListener("click"));
+    }
+    */
 
     
 
     /*
+    NOTES
     saveFile = {
         "saveFileNumber" : saveFileNum,
         "curent_storyLine_progress" : 0,
         "storyLine_progress" : {
             0 :  {"sceneName": "Start",
     */
+       /*
+        "AmountOfButtonsForSceneDefault" : 2,
+        "AmountOfButtonsForScene" : {
+            // scene number : amount of buttons in that scene
+            0: 1, 
+            1: 2,
+        },
+        "Choices_Possible" : {
+        //  chapter number : { scene number : { Button option : Text }}
+        0 : {
+            0 : { 7 : "Next"},       //FIXME change all options from 5 to 7
+            1 : { 1 : "Previously", 7 : "Next",},
+            2 : { 1 : "Previously", 2 : "piercing blue", 3 : "deep brown", 4 : "striking green", 5 : "captivating hazel", 6 : "intense grey", 7 : "Next",}
+        },
 
+        },
+    */
 
-function title_progress(current_title,current_title_progress) {
-    let title_story = current_title['title_story_'+current_title_progress]
-    slowTypingText(title_story,'.Quest_Title');         // Put the content on the left and the place where it needs to go on the right
-    console.log('title_story',title_story)
-    console.log('current_title',current_title);
-}
-function scene_progress(current_storyLine,curent_storyLine_progress,AmountOfButtonsForScene) {
-    let title_scene = current_storyLine['sceneName']
-    let scene_text = current_storyLine['playerText']
-    slowTypingText(scene_text,'.main_section',0, 35);     // Put the content on the left and the place where it needs to go on the right + index + speed
-    console.log('title_scene',title_scene)
-    console.log('scene_text',scene_text)
-    console.log('current_storyLine',current_storyLine);
+    function ButtonPressed(saveFile){
+        if (saveFile.AmountOfButtonsForScene[curent_storyLine_progress] == 1){
+            let Button7 = Button_Choice7;
+            Button7.innerHTML = 'Next';
+            Button7.addEventListener("click", function() {
+                if(isCurrentlyPrinting){
+                    slowTypingText(current_storyLine['playerText'], '.main_section',undefined,undefined,true)
+                    slowTypingText(current_title['title_story_'+current_title_progress], '.main_section',undefined,undefined,true)
+                }else{
+                    console.log("test id=255 ",saveFile.Choices_Made[saveFile.curent_chapter_progress]);
+                    saveFile.Choices_Made[saveFile.curent_chapter_progress].push(5);
+                    console.log("test id=256 ",saveFile.Choices_Made[saveFile.curent_chapter_progress]);
+                    nextScene(saveFile);
+                    
+                }
+            })
+        }
+    }
 
-}
-function nextScene(saveFile){
-    saveFile.curent_storyLine_progress++ ;
-    Title.innerHTML = " ";
-    main_section.innerHTML = " ";
-    choices_section_title.innerHTML = " ";
-    Button_Choice1.innerHTML = " ";  
-    Button_Choice5.innerHTML = " ";
-    story(saveFile);
-    console.log('nextScene');
-}
-function  previousScene(saveFile){
-    saveFile.curent_storyLine_progress-- ;
-    Title.innerHTML = " ";
-    main_section.innerHTML = " ";
-    choices_section_title.innerHTML = " ";
-    Button_Choice1.innerHTML = " ";  
-    Button_Choice5.innerHTML = " ";
-    story(saveFile);
-    console.log('previousScene');
-}
-function nextchapter(saveFile){
-    saveFile.curent_storyLine_progress++ ;
-    saveFile.current_title_progress++ ;
-    Title.innerHTML = " ";
-    main_section.innerHTML = " ";
-    choices_section_title.innerHTML = " ";
-    Button_Choice1.innerHTML = " ";  
-    Button_Choice5.innerHTML = " ";
-    story(saveFile);
-    console.log('nextChapter');
-}
-function  previouschapter(saveFile){
-    saveFile.curent_storyLine_progress-- ;
-    saveFile.current_title_progress-- ;
-    Title.innerHTML = " ";
-    main_section.innerHTML = " ";
-    choices_section_title.innerHTML = " ";
-    Button_Choice1.innerHTML = " ";  
-    Button_Choice5.innerHTML = " ";
-    story(saveFile);
-    console.log('previousChapter');
-}
+    function title_progress(current_title,current_title_progress) {
+        let title_story = current_title['title_story_'+current_title_progress]
+        slowTypingText(title_story,'.Quest_Title');         // Put the content on the left and the place where it needs to go on the right
+        console.log('title_story',title_story)
+        console.log('current_title',current_title);
+    }
+    function scene_progress(current_storyLine,curent_storyLine_progress,AmountOfButtonsForScene) {
+        let title_scene = current_storyLine['sceneName']
+        let scene_text = current_storyLine['playerText']
+        slowTypingText(scene_text,'.main_section',undefined, 35);     // Put the content on the left and the place where it needs to go on the right + index + speed
+        console.log('title_scene',title_scene)
+        console.log('scene_text',scene_text)
+        console.log('current_storyLine',current_storyLine);
+
+    }
+    function nextScene(saveFile){
+        saveFile.curent_storyLine_progress++ ;
+        Title.innerHTML = " ";
+        main_section.innerHTML = " ";
+        choices_section_title.innerHTML = " ";
+        Button_Choice1.innerHTML = " ";  
+        Button_Choice5.innerHTML = " ";
+        story(saveFile);
+        console.log('nextScene');
+    }
+    function  previousScene(saveFile){
+        saveFile.curent_storyLine_progress-- ;
+        Title.innerHTML = " ";
+        main_section.innerHTML = " ";
+        choices_section_title.innerHTML = " ";
+        Button_Choice1.innerHTML = " ";  
+        Button_Choice5.innerHTML = " ";
+        story(saveFile);
+        console.log('previousScene');
+    }
+    function nextchapter(saveFile){
+        saveFile.curent_storyLine_progress++ ;
+        saveFile.current_title_progress++ ;
+        Title.innerHTML = " ";
+        main_section.innerHTML = " ";
+        choices_section_title.innerHTML = " ";
+        Button_Choice1.innerHTML = " ";  
+        Button_Choice5.innerHTML = " ";
+        story(saveFile);
+        console.log('nextChapter');
+    }
+    function  previouschapter(saveFile){
+        saveFile.curent_storyLine_progress-- ;
+        saveFile.current_title_progress-- ;
+        Title.innerHTML = " ";
+        main_section.innerHTML = " ";
+        choices_section_title.innerHTML = " ";
+        Button_Choice1.innerHTML = " ";  
+        Button_Choice5.innerHTML = " ";
+        story(saveFile);
+        console.log('previousChapter');
+    }
 
 
     return saveFile
 }
-function slowTypingText(text, elementId, index = 0, speed = 200) {
+
+function slowTypingText(text, elementId, index = 0, speed = 200, printImmediately = false) {
+    if (printImmediately) {
+        document.querySelector(elementId).innerText = text;
+        isCurrentlyPrinting = false;
+        return;
+    }
     if (index < text.length) {
         document.querySelector(elementId).innerText += text[index];
-        index += 1; // update index for the next character
-
-        // If the current character is a space, replace it with a space character
+        index++;
+        // replace it with a space character
         if (text[index - 1] === " ") {
+            isCurrentlyPrinting = true;
             setTimeout(() => {
                 document.querySelector(elementId).innerText += " "+text[index];
                     index += 1;
@@ -323,38 +429,14 @@ function slowTypingText(text, elementId, index = 0, speed = 200) {
             }, speed);
         } else {
             setTimeout(() => slowTypingText(text, elementId, index, speed), speed);
+            isCurrentlyPrinting = true;
         }
     }
-}
-/*
-function slowType(text, elementId, index = 0, speed = 200, printImmediately = false) {
-    if (printImmediately) {
-        document.querySelector(elementId).innerText = text;
-    } else {
-        if (index < text.length) {
-            document.querySelector(elementId).innerText += text[index];
-            index += 1; // update index for the next character
-
-            // If the current character is a space, replace it with a space character
-            if (text[index - 1] === " ") {
-                setTimeout(() => {
-                    document.querySelector(elementId).innerText += " " + text[index];
-                    index += 1;
-                    slowType(text, elementId, index, speed, printImmediately);
-                }, speed);
-            } else {
-                setTimeout(() => slowType(text, elementId, index, speed, printImmediately), speed);
-            }
-        }
+    if (index == text.length){
+        isCurrentlyPrinting = false;
     }
+    return isCurrentlyPrinting;
 }
-*/
-// Usage example
-const button = document.querySelector("#myButton");
-button.addEventListener("click", () => {
-    slowType("Hello, World!", "#myElement", undefined, undefined, true);
-});
-
 
 INIT()
 function INIT(){
